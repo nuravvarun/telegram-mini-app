336562e0049fd14e6679e5e884449380a5ce2532
nuravvarun <sachdevavarun753@gmail.com>
2024-11-12 01:03:37 +0900

    remove scroll

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ Telegram.WebApp.close();
 
 
 
-const imageHolder = document.getElementById("imageHolder");
-const scrollContent = document.getElementById("scrollContent");
-let lastScroll = 0;
+// const imageHolder = document.getElementById("imageHolder");
+// const scrollContent = document.getElementById("scrollContent");
+// let lastScroll = 0;
 
-window.addEventListener("scroll", () => {
-    const currentScroll = window.scrollY;
+// window.addEventListener("scroll", () => {
+//     const currentScroll = window.scrollY;
 
-    if (currentScroll > lastScroll) {
-        // Scrolling down - hide image and show content
-        imageHolder.classList.add("hide-image");
-        scrollContent.classList.add("show-content");
-    } else {
-        // Scrolling up - show image and hide content
-        imageHolder.classList.remove("hide-image");
-        scrollContent.classList.remove("show-content");
-    }
-    lastScroll = currentScroll;
-});
+//     if (currentScroll > lastScroll) {
+//         // Scrolling down - hide image and show content
+//         imageHolder.classList.add("hide-image");
+//         scrollContent.classList.add("show-content");
+//     } else {
+//         // Scrolling up - show image and hide content
+//         imageHolder.classList.remove("hide-image");
+//         scrollContent.classList.remove("show-content");
+//     }
+//     lastScroll = currentScroll;
+// });
 
 function sendMessage() {
     alert("Message sent to bot!");
