1573957f6de54923eb684ce69f8264024a43cacf
nuravvarun <sachdevavarun753@gmail.com>
2024-12-05 23:20:32 +0900

    files renamed

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -3,10 +3,10 @@ import type { ComponentType, JSX } from 'react';
 import { InitDataPage } from '@/pages/InitDataPage.tsx';
 import { LaunchParamsPage } from '@/pages/LaunchParamsPage.tsx';
 import { ThemeParamsPage } from '@/pages/ThemeParamsPage.tsx';
-import { AuditResult } from '@/pages/auditor/ResultPage';
-import { FraudDetectorResult } from '@/pages/fraud-detector/ResultPage';
-import { RugPullDetectorResult } from '@/pages/rug-pull-detector/ResultPage';
-import { IndexPage } from '@/pages/indexPage/IndexPage';
+import { AuditResult } from '@/pages/auditor/auditResult';
+import { FraudDetectorResult } from '@/pages/fraud-detector/fraud-detector-result';
+import { RugPullDetectorResult } from '@/pages/rug-pull-detector/rug-pull-detector-result';
+import { IndexPage } from '@/pages/index/indexPage';
 
 interface Route {
   path: string;
